4016fd8ef8fcca76b063fff90502c7852aad80ba
Jorge Cuerdo <jocual@gmail.com>
2019-10-29 19:40:13 +0100

    Build

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 rm DiarioTecnologia.apk
 rm app-release-unsigned.apk
+ionic cordova resources android
 ionic cordova build android --release 
 mv platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ./
 #keytool -genkey -v -keystore diariotecnologia.jks -keyalg RSA -keysize 2048 -validity 10000 -alias diariotecnologia
