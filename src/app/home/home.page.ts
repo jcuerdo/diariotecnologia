8a01e3bd9522a8c48121ed51a80ab1e0b26b2c7b
Jorge Cuerdo <jocual@gmail.com>
2019-10-30 15:08:46 +0100

    Add ads

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AdMobFreeInterstitialConfig, AdMobFree } from '@ionic-native/admob-free/ngx';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +14,18 @@ export class HomePage {
   private searchTerm = "";
   private baseURl = "https://www.diariotecnologia.es/api/"
 
-  constructor(private http: HttpClient)  {
+  constructor(private http: HttpClient, private admobFree: AdMobFree
+    )  {
 
   }
 
   ngOnInit(){
-    let promise = this.http.get(this.baseURl);
+    this.loadPosts(0)
+    this.showInterstitialAds()
+  }
+
+  loadPosts(page){
+    let promise = this.http.get(this.baseURl + "?page=" + page );
     promise.subscribe((data : any) => {
       this.posts = data.posts
       this.page++
@@ -28,7 +35,6 @@ export class HomePage {
 
   nextPage(event){
     setTimeout(() => {
-      console.log(this.baseURl + "?page=" + (this.page) )
     let promise = this.http.get(this.baseURl + "?page=" + (this.page) );
     promise.subscribe((data : any) => {
       this.posts = this.posts.concat(data.posts)
@@ -43,9 +49,20 @@ export class HomePage {
 
   refresh(event) {
     setTimeout(() => {
-      this.ngOnInit()
+      this.loadPosts(0)
       event.target.complete();
     }, 10000);
   }
+
+  showInterstitialAds(){
+    let interstitialConfig: AdMobFreeInterstitialConfig = {
+        isTesting: true, // Remove in production
+        autoShow: true,
+        id: "ca-app-pub-6904186947817626/4310930274"
+    };
+    this.admobFree.interstitial.config(interstitialConfig);
+    this.admobFree.interstitial.prepare().then(() => {
+    }).catch(e => alert(e));
+}
 
 }
