a43eb17d0caba333d1ebf92d0f6a521b13312862
Jorge Cuerdo Álvarez <jcuerdo@MacBook-Pro-de-Jorge.local>
2019-11-03 23:16:29 +0100

    Version 13

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ ionic cordova build android --release
 mv platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ./
 #keytool -genkey -v -keystore diariotecnologia.jks -keyalg RSA -keysize 2048 -validity 10000 -alias diariotecnologia
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore diariotecnologia.jks app-release-unsigned.apk diariotecnologia
-~/Library/Android/sdk/build-tools/28.0.1/zipalign -v 4 app-release-unsigned.apk DiarioTecnologia.apk
+~/Library/Android/sdk/build-tools/29.0.0/zipalign -v 4 app-release-unsigned.apk DiarioTecnologia.apk
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AdMobFreeInterstitialConfig, AdMobFree } from '@ionic-native/admob-free/ngx';
+import { AdMobFreeInterstitialConfig, AdMobFree, AdMobFreeRewardVideoConfig } from '@ionic-native/admob-free/ngx';
 
 @Component({
   selector: 'app-home',
@@ -12,57 +12,129 @@ export class HomePage {
   private posts = [];
   private page = 0;
   private searchTerm = "";
-  private baseURl = "https://www.diariotecnologia.es/api/"
+  private baseURl = "https://www.diariotecnologia.es/api/";
+  private idAd = "ca-app-pub-6904186947817626/4310930274";
+  private idVideoAdd = "ca-app-pub-6904186947817626/2154931416"
 
   constructor(private http: HttpClient, private admobFree: AdMobFree
     )  {
-
   }
 
   ngOnInit(){
-    this.loadPosts(0)
-    this.showInterstitialAds()
+    this.loadPosts(false);
   }
 
-  loadPosts(page){
-    let promise = this.http.get(this.baseURl + "?page=" + page );
+  loadPosts(event){
+    let promise = this.http.get(this.baseURl + "?page=" + this.page );
     promise.subscribe((data : any) => {
       this.posts = data.posts
       this.page++
       this.searchTerm = data.searchTerm
+      if(event){
+        event.target.complete();
+      }
     })
   }
 
-  nextPage(event){
-    setTimeout(() => {
-    let promise = this.http.get(this.baseURl + "?page=" + (this.page) );
+  loadMorePosts(event){
+    let promise = this.http.get(this.baseURl + "?page=" + this.page );
     promise.subscribe((data : any) => {
       this.posts = this.posts.concat(data.posts)
       this.page++
       this.searchTerm = data.searchTerm
-      
-      event.target.complete() 
+      if(event){
+        event.target.complete();
+      }
     })
-  }, 10000);
+  }
+
+  nextPage(event){
+    this.showAd();
+    this.loadMorePosts(event);
 
   }
 
   refresh(event) {
-    setTimeout(() => {
-      this.loadPosts(0)
-      event.target.complete();
-    }, 10000);
+    this.showAd();
+    this.page = 0;
+    this.loadPosts(event);
+  }
+
+  showAd(){
+    this.showInterstitialAds();
+  }
+
+  generateRandom(min : number,max : number) : number{
+    let random = Math.round(Math.random() * (+max - +min) + +min); 
+    console.log("Random number generated: " + random)
+    return random;
   }
 
   showInterstitialAds(){
+    console.log("showInterstitialAds")
+
     let interstitialConfig: AdMobFreeInterstitialConfig = {
         isTesting: false, // Remove in production
         autoShow: false,
-        id: "ca-app-pub-6904186947817626/4310930274"
+        id: this.idAd
     };
     this.admobFree.interstitial.config(interstitialConfig);
-    this.admobFree.interstitial.prepare().then(() => {
-    }).catch(e => alert(e));
+
+    this.admobFree.on(this.admobFree.events.INTERSTITIAL_LOAD).subscribe(() => {
+      this.admobFree.interstitial.show().then(() => {
+          console.log('shown')
+      }).catch((errorShow) => {
+        console.log('errror shown')
+        console.log(errorShow)
+      });
+   });
+   
+   this.admobFree.on(this.admobFree.events.INTERSTITIAL_LOAD_FAIL).subscribe((e) => {
+    console.log('on load fail')
+    console.log(e)
+   });
+
+
+   this.admobFree.interstitial.prepare().then(() => {
+    console.log('prepared')
+  }).catch(e => {
+    console.log('fail prepared')
+    console.log(e)
+  });
+   
+}
+
+showVideoAds(){
+  console.log("showVideoAds")
+  let videoConfig: AdMobFreeRewardVideoConfig = {
+      isTesting: false, // Remove in production
+      autoShow: false,
+      id: this.idVideoAdd
+  };
+  this.admobFree.rewardVideo.config(videoConfig);
+
+  this.admobFree.on(this.admobFree.events.REWARD_VIDEO_LOAD).subscribe(() => {
+    this.admobFree.rewardVideo.show().then(() => {
+        console.log('shown')
+    }).catch((errorShow) => {
+      console.log('errror shown')
+      console.log(errorShow)
+    });
+ });
+ 
+ this.admobFree.on(this.admobFree.events.REWARD_VIDEO_LOAD_FAIL).subscribe((e) => {
+  console.log('on load fail')
+  console.log(e)
+ });
+
+
+ this.admobFree.rewardVideo.prepare().then(() => {
+  console.log('prepared')
+}).catch(e => {
+  console.log('fail prepared')
+  console.log(e)
+});
+ 
 }
 
 }
